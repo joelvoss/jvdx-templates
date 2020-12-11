976dcc7ff5d3a0e4aa12a596287250a668fa5bc2
Joel Voss <mail@joelvoss.com>
2020-12-11 08:11:33 +0100

    chore(next-app/deps): update dependencies

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-	future: {
-		removeDeprecatedGapUtilities: true,
-		purgeLayersByDefault: true,
-	},
-	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-	theme: {},
-	variants: {},
-	plugins: [],
-};
+  purge: ['./src/pages/**/*.js', './src/shared/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
