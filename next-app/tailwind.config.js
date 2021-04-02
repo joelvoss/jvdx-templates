4e608b8cc3d18434234060bc4dffaff3aec6e50e
Joel Voss <mail@joelvoss.com>
2021-04-02 20:36:29 +0200

    fix(next-app): fix formatting

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ['./src/pages/**/*.js', './src/shared/**/*.js'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./src/pages/**/*.js', './src/shared/**/*.js'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
