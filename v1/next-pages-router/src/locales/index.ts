0c7882490285909f767c90cf2abb2780967fc413
Joel Voss <mail@joelvoss.com>
2025-07-06 17:32:52 +0200

    feat(v1): remove v1 templates

--- v1/next-pages-router/src/locales/index.ts
+++
@@ -1,8 +0,0 @@
-import DE from './de.json';
-
-export const languages = {
-	de: { locale: 'de', dict: DE },
-};
-
-export const locales = Object.keys(languages);
-export const defaultLocale = locales[0];
--- v1/next-pages-router/tests/jest.shim.js
+++
@@ -1,5 +0,0 @@
-import { TextEncoder } from 'util';
-
-// NOTE(joel): Since Jest v28 the jsdom environment is missing the
-// TextEncoder class we retrofit here.
-global.TextEncoder = TextEncoder;
